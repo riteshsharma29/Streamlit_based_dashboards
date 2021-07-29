e031bff0ab0be75cacfe29c7eba00329b4500370
Ritesh Sharma <riteshsharma29@gmail.com>
2021-07-29 16:51:43 +0530

    Add files via upload

---
+++ dash_cert_report.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import base64
+from openpyxl import load_workbook
+import streamlit as st
+import os
+
+'''
+requires pandas==1.2.4
+'''
+
+def _max_width_():
+    max_width_str = f"max-width: 1900px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+_max_width_()
+
+book = load_workbook(os.path.join("data","report.xlsx"))
+st.title("Report Dashboard")
+appnum=st.selectbox("Select App No.",book.sheetnames)
+
+try:
+    df = pd.read_excel(os.path.join("data","report.xlsx"))
+
+    def highlight_validity(s):
+        return ['background-color: green'] * len(s) if s.Validity > 90 else ['background-color: red'] * len(s)
+
+    def color_validity(val):
+        color = 'green' if val > 90 else 'red'
+        return f'background-color: {color}'
+
+    df.fillna(" ",inplace=True)
+    # df.rename(columns={"Unnamed: 0":"Deployment Env"},inplace=True)
+    df = df [['Status', 'Common Name', 'Valid From', 'Valid To',
+       'Deployment Env', 'Device', 'Owner Email', 'App_Number',
+       'License Status', 'Month', 'Year', 'Validity', 'app']]
+
+    if len(df) > 0:
+        # st.dataframe(df)
+        st.dataframe(df.style.apply(highlight_validity,axis=1))
+        st.dataframe(df.style.applymap(color_validity, subset=["Validity"]))
+except Exception as err:
+    print(err)
+    st.error("No Data Found")
+
+
---
+++ olympic.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+
+st.title('Live Olympics Tokyo 2020 Dashboard')
+st.markdown("Dashboard in Python 🐍 Streamlit! (https://www.streamlit.io/) & Plotly (https://plotly.com/)")
+
+base_df = pd.read_html("https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/medal-standings.htm?utm_campaign=dp_google")
+
+# extracting olympic medal dataset
+df = base_df[0]
+
+# renaming dataframe columns
+df.columns = ['Rank', 'Country', 'Gold', 'Silver', 'Bronze', 'Total','RankbyTotal', 'NOCCode']
+
+# renaming country values to list
+country = df['Country'].to_list()
+
+# combo box
+country_inp = st.selectbox('Select Country from the Dropdown below :', country)
+
+# medal counts
+gold_count = df[df['Country']==country_inp]['Gold'].values
+silver_count = df[df['Country']==country_inp]['Silver'].values
+bronze_count = df[df['Country']==country_inp]['Bronze'].values
+
+res = df[df['Country']==country_inp]
+res.set_index('Rank', inplace=True)
+
+#  creating bar graph
+fig = go.Figure(data=[go.Bar(name='Gold', x=['Gold'], y=gold_count,marker_color='gold'),
+                      go.Bar(name='Silver', x=['Silver'], y=gold_count,marker_color='rgb(180, 180, 180)'),
+                      go.Bar(name='Bronze', x=['Bronze'], y=bronze_count,marker_color='rgb(173, 138, 86)')]
+                )
+
+# creating layout
+
+fig.update_layout(
+    title=country_inp,
+    xaxis_title="Medals",
+    yaxis_title="Scale",
+    legend_title="Medals Tally",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="RebeccaPurple"
+    )
+)
+
+try:
+    st.table(res)
+    st.plotly_chart(fig)
+except Exception as e:
+    st.error(e)
+
+
+
