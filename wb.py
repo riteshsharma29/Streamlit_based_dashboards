f583242ee96fb3e0255b9ca9aa7029bae8e10b32
Ritesh Sharma <ritesh.sharma29@gmail.com>
2021-09-08 20:29:41 +0530

    Add files via upload

---
+++ wb.py
@@ -0,0 +1,12 @@
+import pycountry,mtranslate
+
+print(dir(mtranslate))
+
+# iso = [country.alpha_2 for country in pycountry.countries]
+#
+# for c in iso:
+#     try:
+#         print(pycountry.languages.get(alpha_2=c).name)
+#     except:
+#         continue
+
