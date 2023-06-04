d7f20277a374980911bff0b668ed8c9a16b7e163
Ritesh Sharma <ritesh.sharma29@gmail.com>
2023-06-04 13:11:46 +0530

    Add files via upload

---
+++ chatbot/Home.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(page_title="CHATBOT USING STREAMLIT")
+
+st.markdown("<h3 style='text-align: center; color:BLUE;'>CHATBOT USING PYTHON STREAMLIT FRAMEWORK</h3>",unsafe_allow_html=True)
+
+help_1, help_2 = st.columns(2)
+with help_1:
+    with st.expander("README/ABOUT-app"):
+        st.markdown('''
+        - A simple ChatBot example using Python Streamlit Framework.
+        - No Chatbot module OR library used. Q&A is handled through dictionary of Questions and Answers.  
+        - For demoying have used Python based Questions and Answers.
+        - This just a proof of concept with regards to build a chatbot without any additional module so might not anwsers all questions.
+        - Type in the user input box to initiate the bot.
+        - Type exit or quit in the user input box to terminate the bot.
+        ''')
+
+with help_2:
+    with st.expander("MY CONTACT"):
+        st.markdown('''
+        - LinkedIn : https://www.linkedin.com/in/ritesh-sharma5/
+        - YouTube Channel : https://www.youtube.com/@goautomation            
+        ''')
+
+
+st.markdown("""
+<style>
+     [data-testid=stSidebar] {
+             background-color: #D3D3D3    
+     }
+</style>
+""",unsafe_allow_html=True)
---
+++ chatbot/pages/1_CHATBOT.py
@@ -0,0 +1,75 @@
+import streamlit as st
+
+st.markdown("""
+<style>
+     [data-testid=stAppViewContainer] {
+                 background-color: #87CEFA    
+     }
+</style>
+""",unsafe_allow_html=True)
+
+
+input_text = st.text_input("User: ",key=str(0))
+input_text = input_text.lower().strip()
+
+mydict = {
+    ("hi","hello"):"Hi, I am bot how can I help you",
+    ("python","python language"):"Python is a high-level, general-purpose programming language.\nIts design philosophy emphasizes code readability with the use of significant indentation via the off-side rule.\nPython is dynamically typed and garbage-collected. ",
+    ("web","web development","web development framework"):"Django https://www.djangoproject.com/ is best web development framework in Python.\nFlask https://flask.palletsprojects.com/ is second best",
+    ("modules","pypi"):"Search for any module here https://pypi.org/",
+    ("pip","what is pip"):"pip is python package manager",
+    ("data analysis","pandas","data"): "Best Python data analysis library : https://pandas.pydata.org/",
+    ("ml","machine learning"):"Best Python ML library/module is Scikit-learn https://scikit-learn.org/stable/",
+    ("plots","graph","visualization","plot","charts","chart"):"https://python-charts.com/ - One the best resource avaialble to learn Python data visualization",
+    ("inventor","python inventor","python creator","creator"):"Guido van Rossum best known as the creator of the Python programming language !",
+    ("automation","browser automation","test"): "selenium : https://selenium-python.readthedocs.io/\nRobot Framework : https://robotframework.org/\nPytest : https://docs.pytest.org/en/7.3.x/"
+}
+
+def get_dict_value_by_key(kwd):
+    for keys, v in mydict.items():
+        if kwd in keys:
+            return v
+
+counter = 100
+counter_2 = 1
+
+while (input_text != "") and (input_text != "exit"):
+    try:
+        if get_dict_value_by_key(input_text) != None:
+            output = st.text_area("bot", get_dict_value_by_key(input_text), key=str(counter_2),height=125)
+            input_text = st.text_input("User: ", key=str(counter))
+            input_text = input_text.lower().strip()
+        else:
+            output = st.text_area("bot", "Keyword not found !!! Search with appropriate keyword", key=str(counter_2),height=125)
+            input_text = st.text_input("User: ", key=str(counter))
+            input_text = input_text.lower().strip()
+    except:
+        output = st.text_area("bot", "Keyword not found !!! Search with appropriate keyword", key=str(counter_2),height=125)
+        input_text = st.text_input("User: ", key=str(counter))
+        input_text = input_text.lower().strip()
+
+    if (input_text == "exit") or (input_text == "bye") or ((input_text == "quit")):
+        output = st.text_area("bot", "Thank you for using me ! Goodbye have a nice day ahead.", key=str(counter_2))
+        break
+
+    counter += 100
+    counter_2 += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
